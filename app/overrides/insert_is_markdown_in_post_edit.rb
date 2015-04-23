98f4bc0bd6f366bb0f1eb7bbbb51442e9fdedb53
Tam Vo <vo.mita.ov@gmail.com>
2015-04-23 17:22:40 +0700

    Able to switch markdown and wyswyg editor

--- app/overrides/insert_is_markdown_in_post_edit.rb
+++ app/overrides/insert_is_markdown_in_post_edit.rb
@@ -1,3 +1,7 @@
+Deface::Override.new(:virtual_path => "monologue/admin/posts/new",
+                     :name => "insert_is_markdown_in_post_edit",
+                     :insert_before => "h1",
+                     :partial => "monologue-markdown/overrides/is_markdown")
 Deface::Override.new(:virtual_path => "monologue/admin/posts/edit",
                      :name => "insert_is_markdown_in_post_edit",
                      :insert_before => "h1",
